4abb1ba83fa5966dba95f76d75b9a3f383568c3a
Bala1324 <balachandiran132@gmail.com>
2021-08-27 23:54:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fast2sms = require('fast-two-sms');
+require('dotenv').config();
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
+app.post('/sendMsg',async (req,res)=>{
+    const responce = await fast2sms.sendMessage({authorization: process.env.API_KEY,message: req.body.message,numbers: [req.body.number]});
+res.send(responce);
+})
+
+
+app.get('/',(req,res) =>{
+res.render('index.ejs')
+})
+app.listen(3000,()=>{
+    console.log('Server listening Port 3000');
+    
+})
