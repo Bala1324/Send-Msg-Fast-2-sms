44c56cce50032d27c99c08f3016eb935ad89568f
Bala1324 <balachandiran132@gmail.com>
2021-08-28 00:03:38 +0530

    commit 2

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+
 const fast2sms = require('fast-two-sms');
-require('dotenv').config();
+
+
+const API_KEY = "zV4Kl8LSkEfZ3QtN67vHah1uJCmwjqXAMFs5xiOpPRTce2B9GrFbjte4LYoJzcXM17ZHwKdPTkDSnilA ";
+
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: false}))
+
 app.post('/sendMsg',async (req,res)=>{
-    const responce = await fast2sms.sendMessage({authorization: process.env.API_KEY,message: req.body.message,numbers: [req.body.number]});
+    const responce = await fast2sms.sendMessage({authorization: API_KEY,message: req.body.message,numbers: [req.body.number]});
 res.send(responce);
 })
 
